f94336432aa61856e08610bb65708c7645607d5b
本庄涼真 <20224015-honjoryoma@hcs.ac.jp>
2025-07-07 10:42:52 +0900

    fix: 画像差し替え

--- src/main/java/com/study/LL/healthcare/task/TaskController.java
+++ src/main/java/com/study/LL/healthcare/task/TaskController.java
@@ -1,7 +1,7 @@
 package com.study.LL.healthcare.task;
 
 
-import com.study.xxaxxx.healthcare.user.LoginService;
+import com.study.LL.healthcare.user.LoginService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
